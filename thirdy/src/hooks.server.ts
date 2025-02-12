ad2a38439051e288f78a845a37cc21fa37ec8cfa
Jazoub2 <jakrem@edu.umea.se>
2025-02-12 11:29:02 +0100

    useragent

--- thirdy/src/hooks.server.ts
+++ thirdy/src/hooks.server.ts
@@ -3,7 +3,10 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
     const ip = event.getClientAddress();
     console.log('Client IP:', ip);
+
+    // Get the User-Agent from request headers
+    const userAgent = event.request.headers.get('user-agent');
+    console.log('User-Agent:', userAgent);
     
     return resolve(event);
 };
-    
